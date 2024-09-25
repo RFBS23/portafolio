fec22cb6da588744fbdefd3903086fce9e2f07bd
RFBS18 <fabriziobarrios22@gmail.com>
2024-09-24 19:01:46 -0500

    actualizando

--- components/sections/index/technical.jsx
+++ components/sections/index/technical.jsx
@@ -28,44 +28,65 @@ export default function Technical() {
 		<Section classProp={`${about.section} borderBottom`}>	
 			<Container spacing={['verticalXXXLrg']}>
 				<SectionTitle
-					title="Technical"
-					preTitle="Hardskills"
+					title="Habilidades"
+					preTitle="Mis Habilidades"
 					subTitle="As a creative technologist, I craft intuitive digital experiences using a diverse set of tools and languages."
 				/>
 				<section className={`${about.content} ${about.container}`}>
 					<div className={about.copy}>
-						<CopyBlock 
+						<CopyBlock
 							title="Polymath foundations"
-							icon={[ 'fat', 'chart-network' ]}
+							icon={['fat', 'chart-network']}
 							copy="With a strong foundation in both design and development, I bring a unique perspective to every project and am constantly learning and evolving to stay up-to-date with the latest industry trends."
 							iconClass={about.icon}
 							containerClass={about.container}
 						/>
-						<BadgesBlock 
-							title="Software I love to work with" 
+						<BadgesBlock
+							title="Software I love to work with"
 							copy="With over 20 years of experience creating digital assets, I have a deep and intuitive understanding of how the interfaces of the best software products in the world are designed. This is just some of the software I have used over that time."
 							list={software}
-							block="software" 
+							block="software"
 							fullContainer="fullContainer"
 							icon="grid-2-plus"
 							containerClass={about.container}
-							headerIcon={about.icon} 
+							headerIcon={about.icon}
 						/>
-						<BadgesBlock 
-							title="Technologies I love to build with" 
+						<BadgesBlock
+							title="Technologies I love to build with"
 							copy="I deeply enjoy solving problems through code. From the front end all the way to the back end and the APIs in-between. No project is too challenging for me."
-							list={tech} 
+							list={tech}
 							block="tech"
-							fullContainer="fullContainer" 
+							fullContainer="fullContainer"
 							icon="laptop-code"
 							containerClass={about.container}
-							headerIcon={about.icon} 
-						/>							
+							headerIcon={about.icon}
+						/>
+					</div>
+
+					<div className={about.copy}>
+						<CopyBlock
+							title="Polymath foundations"
+							icon={['fat', 'chart-network']}
+							copy="With a strong foundation in both design and development, I bring a unique perspective to every project and am constantly learning and evolving to stay up-to-date with the latest industry trends."
+							iconClass={about.icon}
+							containerClass={about.container}
+						/>
+						<BadgesBlock
+							title="Software I love to work with"
+							copy="With over 20 years of experience creating digital assets, I have a deep and intuitive understanding of how the interfaces of the best software products in the world are designed. This is just some of the software I have used over that time."
+							list={software}
+							block="software"
+							fullContainer="fullContainer"
+							icon="grid-2-plus"
+							containerClass={about.container}
+							headerIcon={about.icon}
+						/>
 					</div>
 					<div className={`${about.image} ${about.technicalSvg}`}>
-						<Image src="/img/dataism-24.svg" width={477} height={1111} alt="Data Strings 01 by Colorpong: https://ywft.us/2177b695b" />
+						<Image src="/img/dataism-24.svg" width={477} height={1111}
+							   alt="Data Strings 01 by Colorpong: https://ywft.us/2177b695b"/>
 					</div>
-				</section>	
+				</section>
 			</Container>
 			{/* <SectionGridBg gridSize={4}/> */}
 		</Section>
@@ -73,30 +94,30 @@ export default function Technical() {
 }
 
 const software = [
-	{ key: 'photoshop', 	name: 'Photoshop', 			type: 'devicon' },
-	{ key: 'illustrator', 	name: 'Illustrator', 		type: 'devicon' },
-	{ key: 'figma', 		name: 'Figma', 				type: 'devicon' },
-	{ key: 'vscode', 		name: 'VSCode', 			type: 'devicon' },
-	{ key: 'mailbox', 		name: 'Postman', 			type: 'fas' },
-	{ key: 'computer-mouse',name: 'Click Up', 			type: 'fas' },
-	{ key: 'list-music',	name: 'Ableton', 			type: 'fas' },
-	{ key: 'aftereffects',	name: 'After Effects', 		type: 'devicon' },
-	{ key: 'premierepro',	name: 'Premiere Pro', 		type: 'devicon' },
+	{key: 'photoshop', name: 'Photoshop', type: 'devicon'},
+	{key: 'illustrator', name: 'Illustrator', type: 'devicon'},
+	{key: 'figma', name: 'Figma', type: 'devicon'},
+	{key: 'vscode', name: 'VSCode', type: 'devicon'},
+	{key: 'mailbox', name: 'Postman', type: 'fas'},
+	{key: 'computer-mouse', name: 'Click Up', type: 'fas'},
+	{key: 'list-music', name: 'Ableton', type: 'fas'},
+	{key: 'aftereffects', name: 'After Effects', type: 'devicon'},
+	{key: 'premierepro', name: 'Premiere Pro', type: 'devicon'},
 ]
 
-const tech	= [
-	{ key: 'javascript', 	name: 'JavaScript', 		type: 'devicon' },
-	{ key: 'nodejs', 		name: 'NodeJS', 			type: 'devicon' },
-	{ key: 'react', 		name: 'React', 				type: 'devicon' },
-	{ key: 'nextjs', 		name: 'NextJS', 			type: 'devicon' },
-	{ key: 'jquery', 		name: 'jQuery', 			type: 'devicon' },
-	{ key: 'php', 			name: 'PHP', 				type: 'devicon' },
-	{ key: 'wordpress', 	name: 'WordPress', 			type: 'devicon' },
-	{ key: 'woocommerce', 	name: 'WooCommerce', 		type: 'devicon' },
-	{ key: "google",		name: "GA4/GTM", 			type: "devicon" },
-	{ key: 'html5', 		name: 'HTML5', 				type: 'devicon' },
-	{ key: 'css3', 			name: 'CSS3', 				type: 'devicon' },
-	{ key: 'sass', 			name: 'SASS', 				type: 'devicon' },
+const tech = [
+	{key: 'javascript', name: 'JavaScript', type: 'devicon'},
+	{key: 'nodejs', name: 'NodeJS', type: 'devicon'},
+	{key: 'react', name: 'React', type: 'devicon'},
+	{key: 'nextjs', name: 'NextJS', type: 'devicon'},
+	{key: 'jquery', name: 'jQuery', type: 'devicon'},
+	{key: 'php', name: 'PHP', type: 'devicon'},
+	{key: 'wordpress', name: 'WordPress', type: 'devicon'},
+	{key: 'woocommerce', name: 'WooCommerce', type: 'devicon'},
+	{key: "google", name: "GA4/GTM", type: "devicon"},
+	{key: 'html5', name: 'HTML5', type: 'devicon'},
+	{key: 'css3', name: 'CSS3', type: 'devicon'},
+	{key: 'sass', name: 'SASS', type: 'devicon' },
 	{ key: 'git', 			name: 'Git', 				type: 'devicon' },
 	{ key: 'mysql', 		name: 'MySQL', 				type: 'devicon' },
 	{ key: 'mongodb', 		name: 'MongoDB', 			type: 'devicon' },
